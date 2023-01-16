d91d593fac59919185a30dec1d72b1864dccf3a8
Jitendra Kumar <jitendrakumarbunkar85@gmail.com>
2023-01-17 00:53:48 +0530

    ready for deploy

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose=require("mongoose");
+const mongoURI="mongodb+srv://root:root022@cluster0.rq55vdi.mongodb.net/inotebook"
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected To Mongo Successfully")
+    })
+}
+
+module.exports=connectToMongo;
