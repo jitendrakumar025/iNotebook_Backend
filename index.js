42bd19f62571085aee30ff7c56f902a5c87539b0
Jitendra Kumar <jitendrakumarbunkar85@gmail.com>
2023-02-04 15:30:59 +0530

    cors sorted

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express()
 const port = 5000
 const cors=require('cors')
 
-app.use(cors())
+app.use(cors({ 
+ 
+    origin:[ "http://localhost:3000","https://inotebook-te8u.onrender.com"
+      ]
+}))
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send("Hello From Backend")
